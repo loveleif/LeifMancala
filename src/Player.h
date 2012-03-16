596ee040da4b228a1178cc5899c12798388624d8
Kristofer Leifland <peoplesbeer@gmail.com>
2012-03-16 12:34:05 +0100

    Added Player.

---
+++ src/Player.h
@@ -0,0 +1,32 @@
+/*
+ * Player.h
+ *
+ *  Created on: Mar 16, 2012
+ *      Author: toffe
+ */
+
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+#include <string>
+
+using namespace std;
+
+class Player {
+	const string name;
+
+public:
+	struct Move {
+		const Player &player;
+		const int pitIndex;
+
+		Move(const Player &player, int pitIndex) : player(player), pitIndex(pitIndex) { }
+	};
+
+	Player(const string &playerName);
+	virtual ~Player();
+
+	virtual Player::Move getNextMove() = 0;
+};
+
+#endif /* PLAYER_H_ */
