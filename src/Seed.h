c8c4552c0014fb1815b05f6f6f682ac7f6175993
Kristofer Leifland <peoplesbeer@gmail.com>
2012-03-16 11:42:23 +0100

    Added Seed.

---
+++ src/Seed.h
@@ -0,0 +1,26 @@
+/*
+ * Seed.h
+ *
+ *  Created on: Mar 16, 2012
+ *      Author: Kristofer Leifland
+ */
+
+#ifndef SEED_H_
+#define SEED_H_
+
+class Seed {
+	int value;
+	const static int DEFAULT_VALUE = 1;
+public:
+	Seed(int value = DEFAULT_VALUE) {
+		this->value = value;
+	}
+
+	virtual ~Seed() { }
+
+	int getValue() {
+		return value;
+	}
+};
+
+#endif /* SEED_H_ */
