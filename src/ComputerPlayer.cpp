24edea6d36ae5f63a46ff124a967a84fd8b4fc04
Kristofer Leifland <peoplesbeer@gmail.com>
2012-03-17 21:01:53 +0100

    Added ComputerPlayer.

---
+++ src/ComputerPlayer.cpp
@@ -0,0 +1,47 @@
+/*
+ * ComputerPlayer.cpp
+ *
+ *  Created on: Mar 17, 2012
+ *      Author: toffe
+ */
+
+#include "ComputerPlayer.h"
+#include <time.h>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+unsigned int ComputerPlayer::instanceCount = 0;
+
+const char* ComputerPlayer::names[] =
+	{"HAL 9000", "Deep Thought", "Marvin", "MU-TH-R",
+	 "Bender Bending Rodríguez", "GLaDOS", "Ash", "Bishop", "Project 2501",
+	 "C-3PO", "R2-D2", "K-9", "Dr. Know", "Skynet", "T-1000"};
+
+ComputerPlayer::ComputerPlayer() : super(whatsMyName()) {
+	++instanceCount;
+}
+
+/*
+ *  Returns this instances' name. The order of the available names are randomized
+ *  at first instantiation and then new instantiations gets names according to
+ *  the randomized order.
+ */
+const char* ComputerPlayer::whatsMyName() {
+	if (instanceCount == 0) {
+		// Shuffle names
+		srand(time(NULL));
+		for (int i = 0; i < NAMES_SIZE; ++i)
+			swap(names[i], names[rand() % (NAMES_SIZE - i) + i]);
+	}
+	return names[(instanceCount) % NAMES_SIZE];
+}
+
+ComputerPlayer::~ComputerPlayer() {
+	// TODO Auto-generated destructor stub
+}
+
+Player::Move ComputerPlayer::getNextMove() {
+
+}
---
+++ src/ComputerPlayer.h
@@ -0,0 +1,29 @@
+/*
+ * ComputerPlayer.h
+ *
+ *  Created on: Mar 17, 2012
+ *      Author: toffe
+ */
+
+#ifndef COMPUTERPLAYER_H_
+#define COMPUTERPLAYER_H_
+#define NAMES_SIZE 15
+
+#include "Player.h"
+#include <string>
+#include <vector>
+class ComputerPlayer: public Player {
+	typedef Player super;
+
+	const static char* names[NAMES_SIZE];
+	static unsigned int instanceCount;
+
+	const char* whatsMyName();
+public:
+	ComputerPlayer();
+	virtual ~ComputerPlayer();
+
+	Player::Move getNextMove();
+};
+
+#endif /* COMPUTERPLAYER_H_ */
