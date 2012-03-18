1c56f64885138942adbae55a4fbbb5795ed653b3
Kristofer Leifland <peoplesbeer@gmail.com>
2012-03-18 07:15:18 +0100

    Added RandomPlayer.

---
+++ src/players/RandomPlayer.cpp
@@ -0,0 +1,21 @@
+/*
+ * RandomPlayer.cpp
+ *
+ *  Created on: Mar 18, 2012
+ *      Author: toffe
+ */
+
+#include "RandomPlayer.h"
+
+RandomPlayer::RandomPlayer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RandomPlayer::~RandomPlayer() {
+	// TODO Auto-generated destructor stub
+}
+
+Player::Move RandomPlayer::getNextMove() {
+
+}
