1941a2a98c490caa9c49d498b914ffa54ba03338
Kristofer Leifland <peoplesbeer@gmail.com>
2012-03-19 19:38:31 +0100

    Testing.

--- src/LeifMancala.cpp
+++ src/LeifMancala.cpp
@@ -26,27 +26,36 @@ int main() {
 
 
 	string name ("Human");
+	string name2 ("Human 2");
 	Player *p1 = new HumanPlayer (name);
-	Player *p2 = new RandomPlayer ();
+	//Player *p2 = new RandomPlayer ();
+	Player *p2 = new HumanPlayer (name2);
 	vector<Player*> players;
 	players.push_back(p1);
 	players.push_back(p2);
 
-
+	int pi;
 	Player::Move *move;
 	Board *board = new StandardBoard(players, 7, 5);
 	while (!board->isGameOver()) {
 		cout << *board << "\n\n";
 
 		if (board->isMyTurn(*p1)) {
-			move = new Player::Move(*p1, readInt());
+			//pi = readInt();
+			cout << "Player 1: ";
+			cin >> pi;
+			move = new Player::Move(*p1, pi);
 			board->move(*move);
 		} else {
-			board->move(*p2->getNextMove(*board));
+			//board->move(*p2->getNextMove(*board));
+			//pi = readInt();
+			cout << "Player 2: ";
+			cin >> pi;
+			move = new Player::Move(*p2, pi);
+			board->move(*move);
 		}
-
 	}
-
+	cout << "Game Over!" << endl;
 
 	delete board;
 	delete move;
