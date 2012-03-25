900d1f936440cee42030872c49377b4dee23eee2
Kristofer Leifland <peoplesbeer@gmail.com>
2012-03-25 09:11:59 +0200

    Commenting.

--- src/Pit.h
+++ src/Pit.h
@@ -26,15 +26,54 @@ public:
 	Pit(const Player &owner, bool isStore);
 	virtual ~Pit();
 
+	/*
+	 * Add seed to this pit.
+	 */
 	void add(Seed seed);
+
+	/*
+	 * Removes the last added seed from the pit and returns (a copy of) it.
+	 */
 	Seed pop();
+
+	/*
+	 * Pops and add the popped seed to the specified pit.
+	 */
 	void popAndPush(Pit &pushPit);
+
+	/*
+	 * Pops all seed in this pit and moves it to the specified pit.
+	 */
 	void popAndPushAll(Pit &pushPit);
+
+	/*
+	 * Returns true if there's no seed in this pit.
+	 */
 	bool isEmpty() const;
+
+	/*
+	 * Returns true if this is a store.
+	 */
 	bool isStore() const;
+
+	/*
+	 * Returns the owner of this pit.
+	 */
 	const Player& getOwner() const;
+
+	/*
+	 * Returns the value of the seed in this pit.
+	 */
 	int getValue() const;
+
+	/*
+	 * Return the number of seed in this pit.
+	 */
 	int getSeedCount() const;
+
+	/*
+	 * Returns string representation of the pit.
+	 */
 	std::string toString() const;
 };
 
